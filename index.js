e31f520fdf2af0b8b97b0ceb725947243e8fb03b
Zander Huff <z@zanderhuff.io>
2020-01-30 20:39:17 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const musicians = ["John Lennon", "Paul McCartney", "Tommy Moore"]
 var theBeatlesPlay = function (musicians, instruments) {
   let newList = []
   for (let i = 0; i>musicians.length; i++) {
+    newlist += musicians + " plays " + instruments
     
   }
   
