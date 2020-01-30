1b8b471102d87bfe964654164168f1ddef436019
Zander Huff <z@zanderhuff.io>
2020-01-30 20:48:19 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,8 +2,7 @@
 function theBeatlesPlay(musicians, instruments) {
   var array = []
   for (var i = 0; i < musicians.length; i++) {
-    newlist += musicians + " plays " + instruments + " ."
+    array.push(musicians + " plays " + instruments + " .") 
   }
+  return array;
 }
-
-console.log(theBeatlesPlay())
