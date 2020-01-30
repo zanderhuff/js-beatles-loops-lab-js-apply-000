2c2acb96ad92353067e30197397bc95b0d9bbeb4
Zander Huff <z@zanderhuff.io>
2020-01-30 20:47:42 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // add solution here
 function theBeatlesPlay(musicians, instruments) {
   var array = []
-  for (let i = 0; i < musicians.length; i++) {
+  for (var i = 0; i < musicians.length; i++) {
     newlist += musicians + " plays " + instruments + " ."
   }
 }
