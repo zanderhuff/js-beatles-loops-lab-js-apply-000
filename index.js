04b8d4af34b7d251efed460e99f9c2891a0118a5
Zander Huff <z@zanderhuff.io>
2020-01-30 21:03:14 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -19,5 +19,9 @@ function johnLennonFacts(facts) {
 
 function iLoveTheBeatles(num) {
   var array = [];
-  
+  var num = 0
+  do { 
+    array.push("I love the Beatles!");
+  } while (num < 0);
+  return array;
 }
