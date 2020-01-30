ad5eb0b0c0dbaf6281893a5304faaf168b426321
Zander Huff <z@zanderhuff.io>
2020-01-30 20:46:52 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,7 +1,4 @@
 // add solution here
-const instruments = ["Guitar", "Bass Guitar", "Lead Guitar", "Drums"]
-const musicians = ["John Lennon", "Paul McCartney", "George Harrison", "Ringo Starr"]
-
 var theBeatlesPlay = function (musicians, instruments) {
   let newList = []
   for (let i = 0; i>musicians.length; i++) {
