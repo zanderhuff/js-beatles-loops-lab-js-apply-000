20e9ee561ba45ac6d3ea3324712ef42a22999915
Zander Huff <z@zanderhuff.io>
2020-01-30 20:20:00 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // add solution here
+var instruments = []
+var musicians = []
+
+var theBeatlesPlay = function (musicians, instruments) {
+  var var1 = []
+  for (i in musicians; i > 1; i++)
+  
+  
+}
