d236107f66fe796da90e1a5198d63552d6296c1e
Zander Huff <z@zanderhuff.io>
2020-01-30 20:47:03 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // add solution here
 var theBeatlesPlay = function (musicians, instruments) {
-  let newList = []
+  var array = []
   for (let i = 0; i>musicians.length; i++) {
     newlist += musicians + " plays " + instruments + " ."
   }
